5449d29e4c9a70e6d4d2b8f2e842f88ea89ab0f3
Pavel Schuklin <pavel.shchuklin@homecredit.ru>
2021-07-29 23:18:55 +0400

    MVC

---
+++ StudentFiles/Model/Student.swift
@@ -0,0 +1,14 @@
+//
+//  Student.swift
+//  StudentFiles
+//
+//  Created by Pasha on 27.07.2021.
+//
+
+import Foundation
+
+struct Student {
+    var lastName: String
+    var firstName: String
+    var averageScore: Int
+}
